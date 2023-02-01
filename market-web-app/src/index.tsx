1d4a706f0c2a56f7939bef87602fda06e0d8b74a
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2023-02-01 20:33:21 +0545

    initialize apollo-client

--- market-web-app/src/index.tsx
+++ market-web-app/src/index.tsx
@@ -5,12 +5,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+const client = new ApolloClient({
+  uri: `http://localhost:4000/`,
+  cache: new InMemoryCache(),
+});
+
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>
 );
